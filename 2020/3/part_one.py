60fbd23dc830ab56703878896b4c69e7ca3e572a
fffffrolov <frolowroman@gmail.com>
2020-12-03 21:49:38 +0700

    day 3 part 1

---
+++ 2020/3/part_one.py
@@ -0,0 +1,29 @@
+import sys
+from pathlib import Path
+
+
+def get_next_index(index: int, width: int) -> int:
+	return (index + 3) % width
+
+
+def slither() -> int:
+	trees = 0
+	index = 0
+	started = False
+	with Path('./input.txt').open() as input_file:
+		for line in input_file.readlines():
+			line = line.strip()
+			if not started: 
+				started = True
+				continue
+			
+			index = get_next_index(index, len(line))
+			if line[index] == '#':
+				trees += 1
+
+	return trees
+
+
+if __name__ == '__main__':
+	result = slither()
+	sys.stdout.write(f'Total: {result}\n')
